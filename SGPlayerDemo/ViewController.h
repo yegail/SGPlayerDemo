08eb9ae1dba5f8db0f65ae0b75cd69585ecced12
ZhouTing <642182927@qq.com>
2019-05-09 13:56:23 +0800

    Initial Commit

---
+++ SGPlayerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SGPlayerDemo
+//
+//  Created by TRS on 2019/5/9.
+//  Copyright © 2019 yegail. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SGPlayerDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  SGPlayerDemo
+//
+//  Created by TRS on 2019/5/9.
+//  Copyright © 2019 yegail. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
